6a02c7b70a4f06d3528fef189311510d959d117b
Linux99 <genesiskikimba@gmail.com>
2025-05-15 14:53:41 +0300

    login errors

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,8 +10,8 @@
                         <div class="mb-4 text-center">
                             <img src="{{ asset('assets/ui/images/logo.png')}}" alt="" class=" h-30px h-md-40px"
                                 height="200" />
-                            <p class="fs-18 text-primary mb-0 text-uppercase fw-700">__("Connection")</p>
-                            <p class="my-2 opacity-60">__("Connectez-vous avec votre propre compte")</p>
+                            <p class="fs-18 text-primary mb-0 text-uppercase fw-700">{{__("Connection")}}</p>
+                            <p class="my-2 opacity-60">{{__("Connectez-vous avec votre propre compte")}}</p>
                         </div>
                         <form class="pad-hor" method="POST" role="form" action="{{route('login')}}">
                             @csrf
@@ -36,27 +36,27 @@
                                     <div class="text-left">
                                         <label class="aiz-checkbox">
                                             <input type="checkbox" name="remember" id="remember">
-                                            <span class="opacity-60 text-lowercase">__("Se souvenir de moi")</span>
+                                            <span class="opacity-60 text-lowercase">{{__("Se souvenir de moi")}}</span>
                                             <span class="aiz-square-check"></span>
                                         </label>
                                     </div>
                                 </div>
                                 <div class="col-6">
                                     <div class="text-right opacity-100">
-                                        <a href="/" class="text-reset text-lowercase">__("Mot de passe oublie?")</a>
+                                        <a href="/" class="text-reset text-lowercase">{{__("Mot de passe oublie?")}}</a>
                                     </div>
                                 </div>
                             </div>
                             <button type="submit" class="btn btn-primary btn-lg btn-block">
-                                __("Se connecter")
+                              {{  __("Se connecter")}}
                             </button>
                         </form>
                         <div class="mt-4 text-center">
-                            <a href="/contact"> __("Contactez-nous pour avoir plus d'information")</a>
+                            <a href="/contact"> {{__("Contactez-nous pour avoir plus d'information")}}</a>
                         </div>
                         <div class="mt-4">
                             <p class="m-0 p-0 text-center">
-                                __("Propulse par Linux99")
+                               {{ __("Propulse par Linux99")}}
                             </p>
                         </div>
                     </div>
