15b52fb318ce9c25e6266d8a382bcb540ed15bdc
Linux99 <genesiskikimba@gmail.com>
2025-05-15 16:23:02 +0300

    login errors 5

--- resources/views/config/autres.blade.php
+++ resources/views/config/autres.blade.php
@@ -6,11 +6,11 @@
         <div class="aiz-titlebar text-left mt-2 mb-3">
             <div class="row align-items-center">
                 <div class="col-md-6">
-                    <h1 class="h3">__("Site Configuration")</h1>
+                    <h1 class="h3">{{__("Site Configuration")}}</h1>
                 </div>
                 <div class="col-md-6 text-md-right">
                     <a href="{{ route('staff.create')}}" class="btn btn-circle btn-info">
-                        <span>__("Voir les modifications")</span>
+                        <span>{{__("Voir les modifications")}}</span>
                     </a>
                 </div>
             </div>
@@ -22,18 +22,18 @@
             <div class="row my-2 card">
                 <div class="card-header row gutters-5">
                     <div class="col text-center text-md-left">
-                        <h5 class="mb-md-0 h6">__("Page d'acceuil Configutation")</h5>
+                        <h5 class="mb-md-0 h6">{{__("Page d'acceuil Configutation")}}</h5>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6">
                         <div class="card-body">
                             <div class="">
-                                <label for="inputNumeroligne" class="control-label">__('Images de publicite')</label>
+                                <label for="inputNumeroligne" class="control-label">{{__('Images de publicite')}}</label>
                                 <input type="file" name="pub_images[]" id="inputNumeroligne" class="form-control" value="" title="" multiple>
                             </div>
                             <div class="">
-                                <label for="inputNumeroligne" class="control-label">__('Images Centrales')</label>
+                                <label for="inputNumeroligne" class="control-label">{{__('Images Centrales')}}</label>
                                 <input type="file" name="center_images[]" id="inputNumeroligne" class="form-control" value="" title="" multiple>
                             </div>
                         </div>
@@ -41,7 +41,7 @@
                     <div class="col-md-6 ">
                         <div class="card-body">
                             <div class="">
-                                <label for="inputNumeroligne" class="control-label">__('Publicite Textuel')</label>
+                                <label for="inputNumeroligne" class="control-label">{{__('Publicite Textuel')}}</label>
                                 <input type="text" name="publicite_txt" id="inputNumeroligne" class="form-control" value="{{ $config->publicite_txt ?? ''}}" title="">
                             </div>
                         </div>
@@ -51,10 +51,10 @@
             <div class="col-12 my-3">
                 <div class="btn-toolbar float-right mb-3" role="toolbar" aria-label="Toolbar with button groups">
                     <div class="btn-group mr-2" role="group" aria-label="Third group">
-                        <button type="submit" name="button" value="unpublish" class="btn btn-primary action-btn">__("Enregistrer")</button>
+                        <button type="submit" name="button" value="unpublish" class="btn btn-primary action-btn">{{__("Enregistrer")}}</button>
                     </div>
                     <div class="btn-group" role="group" aria-label="Second group">
-                        <button type="submit" name="button" value="publish" class="btn btn-success action-btn">__("Enregistrer et publier")</button>
+                        <button type="submit" name="button" value="publish" class="btn btn-success action-btn">{{__("Enregistrer et publier")}}</button>
                     </div>
                 </div>
             </div>
@@ -65,13 +65,13 @@
     <div class="modal-dialog modal-sm modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title h6">__("Confirmation de suppression")</h4>
+                <h4 class="modal-title h6">{{__("Confirmation de suppression")}}</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
             </div>
             <div class="modal-body text-center">
-                <p class="mt-1">__("Voulez-vous vraiment supprimer ce membre?")</p>
-                <button type="button" class="btn btn-link mt-2" data-dismiss="modal">__("Annuler")</button>
-                <a href="" class="btn btn-primary mt-2 comfirm-link" id="supp" data-id="">__("Supprimer")</a>
+                <p class="mt-1">{{__("Voulez-vous vraiment supprimer ce membre?")}}</p>
+                <button type="button" class="btn btn-link mt-2" data-dismiss="modal">{{__("Annuler")}}</button>
+                <a href="" class="btn btn-primary mt-2 comfirm-link" id="supp" data-id="">{{__("Supprimer")}}</a>
             </div>
         </div>
     </div>
--- resources/views/config/entreprise.blade.php
+++ resources/views/config/entreprise.blade.php
@@ -6,11 +6,11 @@
         <div class="aiz-titlebar text-left mt-2 mb-3">
             <div class="row align-items-center">
                 <div class="col-md-6">
-                    <h1 class="h3">__("Site Configuration")</h1>
+                    <h1 class="h3">{{__("Site Configuration")}}</h1>
                 </div>
                 <div class="col-md-6 text-md-right">
                     <a href="{{ route('staff.create')}}" class="btn btn-circle btn-info">
-                        <span>__("Voir les modifications")</span>
+                        <span>{{__("Voir les modifications")}}</span>
                     </a>
                 </div>
             </div>
@@ -22,31 +22,31 @@
                 <div class="card">
                     <div class="card-header row gutters-5">
                         <div class="col text-center text-md-left">
-                            <h5 class="mb-md-0 h6">__("Entreprise Info")</h5>
+                            <h5 class="mb-md-0 h6">{{__("Entreprise Info")}}</h5>
                         </div>
                     </div>
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-6">
-                                <label for="inputNumeroligne" class="control-label">__('Nom de
-                                    l\'Entreprise')</label>
+                                <label for="inputNumeroligne" class="control-label">{{__('Nom de
+                                    l\'Entreprise')}}</label>
                                 <input type="text" class="form-control" value="{{Config('app.name')}}" disabled>
                             </div>
                             <div class="col-md-6">
-                                <label for="inputNumeroligne" class="control-label">__('Adresse physique')</label>
+                                <label for="inputNumeroligne" class="control-label">{{__('Adresse physique')}}</label>
                                 <input type="text" name="adresse" class="form-control"
                                     value="{{ $config->adresse ?? ''}}"
-                                    placeholder="__('009 Av. du Julien Paluku, Q. Murara, Com. Karisimbi, Goma')">
+                                    placeholder="{{__('009 Av. du Julien Paluku, Q. Murara, Com. Karisimbi, Goma')}}">
                             </div>
                             <div class="col-md-6">
-                                <label for="inputNumeroligne" class="control-label">__('Numero RCCM')</label>
+                                <label for="inputNumeroligne" class="control-label">{{__('Numero RCCM')}}</label>
                                 <input type="numero" class="form-control" name="rccm" value="{{ $config->rccm ?? ''}}"
-                                    title="__('Ecrivez le numero RCCM de l\'entreprise')">
+                                    title="{{__('Ecrivez le numero RCCM de l\'entreprise')}}">
                             </div>
                             <div class="col-md-6">
-                                <label for="inputNumeroligne" class="control-label">__('Boite Postal')</label>
+                                <label for="inputNumeroligne" class="control-label">{{__('Boite Postal')}}</label>
                                 <input type="text" name="bp" class="form-control" value="{{ $config->bp ?? ''}}"
-                                    title="__('Ecrivez l\'adresse intagram de votre site')">
+                                    title="{{__('Ecrivez l\'adresse intagram de votre site')}}">
                             </div>
                         </div>
                     </div>
@@ -56,36 +56,36 @@
                 <div class="card">
                     <div class="card-header row gutters-5">
                         <div class="col text-center text-md-left">
-                            <h5 class="mb-md-0 h6">__("Reseaux sociaux")</h5>
+                            <h5 class="mb-md-0 h6">{{__("Reseaux sociaux")}}</h5>
                         </div>
                     </div>
                     <div class="card-body">
                         <div class="">
-                            <label for="inputNumeroligne" class="control-label">__('Facebook')</label>
+                            <label for="inputNumeroligne" class="control-label">{{__('Facebook')}}</label>
                             <input type="text" name="facebook" id="inputNumeroligne" class="form-control"
                                 value="{{ $config->facebook ?? ''}}"
-                                title="__('Ecrivez l\'adresse de facebook de votre site')">
+                                title="{{__('Ecrivez l\'adresse de facebook de votre site')}}">
                         </div>
                         <div class="">
-                            <label for="inputNumeroligne" class="control-label">__('Instagram')</label>
+                            <label for="inputNumeroligne" class="control-label">{{__('Instagram')}}</label>
                             <input type="text" name="instagrame" id="inputNumeroligne" class="form-control"
                                 value="{{ $config->instagrame ?? ''}}"
-                                title="__('Ecrivez l\'adresse intagram de votre site')">
+                                title="{{__('Ecrivez l\'adresse intagram de votre site')}}">
                         </div>
                         <div class="">
-                            <label for="inputNumeroligne" class="control-label">__('Twitter')</label>
+                            <label for="inputNumeroligne" class="control-label">{{__('Twitter')}}</label>
                             <input type="text" name="twitter" id="inputNumeroligne" class="form-control"
                                 value="{{ $config->twitter ?? ''}}"
-                                title="__('Ecrivez l\'adresse de twitter de votre site')">
+                                title="{{__('Ecrivez l\'adresse de twitter de votre site')}}">
                         </div>
                         <div class="">
-                            <label for="inputNumeroligne" class="control-label">__('Linkdin')</label>
+                            <label for="inputNumeroligne" class="control-label">{{__('Linkdin')}}</label>
                             <input type="text" name="linkdin" id="inputNumeroligne" class="form-control"
                                 value="{{ $config->linkdin ?? ''}}"
-                                title="__('Ecrivez l\'adresse linkdin  de votre page')">
+                                title="{{__('Ecrivez l\'adresse linkdin  de votre page')}}">
                         </div>
                         <div class="">
-                            <label for="inputNumeroligne" class="control-label">__('Youtube')</label>
+                            <label for="inputNumeroligne" class="control-label">{{__('Youtube')}}</label>
                             <input type="text" name="youtube" id="inputNumeroligne" class="form-control"
                                 value="{{ $config->youtube ?? ''}}" title="">
                         </div>
@@ -94,34 +94,34 @@
                 <div class="mx-2 card">
                     <div class="card-header row gutters-5">
                         <div class="col text-center text-md-left">
-                            <h5 class="mb-md-0 h6">__("Autres contacts du site")</h5>
+                            <h5 class="mb-md-0 h6">{{__("Autres contacts du site")}}</h5>
                         </div>
                     </div>
                     <div class="card-body">
                         <div class="">
-                            <label for="inputNumeroligne" class="control-label">__('Numero de la ligne
-                                prive')</label>
+                            <label for="inputNumeroligne" class="control-label">{{__('Numero de la ligne
+                                prive')}}</label>
                             <input type="tel" name="numeropv" id="inputNumeroligne" class="form-control"
                                 value="{{ $config->numeropv ?? ''}}" title="">
                         </div>
                         <div class="">
-                            <label for="inputNumeroligne" class="control-label">__('Numero whatsapp')</label>
+                            <label for="inputNumeroligne" class="control-label">{{__('Numero whatsapp')}}</label>
                             <input type="text" name="whatsapp" id="inputNumeroligne" class="form-control"
                                 value="{{ $config->whatsapp ?? ''}}" title="">
                         </div>
                         <div class="">
-                            <label for="inputNumeroligne" class="control-label">__('Numero/Lien Telegram')</label>
+                            <label for="inputNumeroligne" class="control-label">{{__('Numero/Lien Telegram')}}</label>
                             <input type="text" name="telegram" id="inputNumeroligne" class="form-control"
                                 value="{{ $config->telegram ?? ''}}" title="">
                         </div>
                         <div class="">
-                            <label for="inputNumeroligne" class="control-label">__('Email de l\'entreprise')</label>
+                            <label for="inputNumeroligne" class="control-label">{{__('Email de l\'entreprise')}}</label>
                             <input type="text" name="email" id="inputNumeroligne" class="form-control"
                                 value="{{ $config->email ?? ''}}" title="">
                         </div>
                         <div class="">
-                            <label for="inputNumeroligne" class="control-label">__('Adresse de
-                                l\'entreprise')</label>
+                            <label for="inputNumeroligne" class="control-label">{{__('Adresse de
+                                l\'entreprise')}}</label>
                             <input type="text" name="adresse" id="inputadresse" class="form-control"
                                 value="{{ $config->adresse ?? ''}}" title="">
                         </div>
@@ -132,11 +132,11 @@
                 <div class="btn-toolbar float-right mb-3" role="toolbar" aria-label="Toolbar with button groups">
                     <div class="btn-group mr-2" role="group" aria-label="Third group">
                         <button type="submit" name="button" value="unpublish"
-                            class="btn btn-primary action-btn">__("Enregistrer")</button>
+                            class="btn btn-primary action-btn">{{__("Enregistrer")}}</button>
                     </div>
                     <div class="btn-group" role="group" aria-label="Second group">
                         <button type="submit" name="button" value="publish"
-                            class="btn btn-success action-btn">__("Enregistrer et publier")</button>
+                            class="btn btn-success action-btn">{{__("Enregistrer et publier")}}</button>
                     </div>
                 </div>
             </div>
